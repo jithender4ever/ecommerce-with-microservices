102b78354edd712002eb16b900904d4792f3a47f
Jithender Reddy P <Jithender_R_Pakanati@homedepot.com>
2020-06-14 15:36:19 -0400

    Implemented signin and currentuser routes.

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Currentuser Router");
+  // This if is equivalent to "!req.session || !req.session.jwt"
+  if (!req.session?.jwt) {
+    res.send({ currentuser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session?.jwt, JWT_SECRET);
+    res.send({ currentUser: payload });
+  } catch (e) {
+    res.send({ currentuser: null });
+  }
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,51 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/badRequestError";
+import { validateRequest } from "../middlewares/validateRequest";
+import { Password } from "../utils/password";
+import { JWT_SECRET } from "../config";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/signin", (req, res) => {
-  res.send("Signin Router");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("Password cannot be empty"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new BadRequestError("Invalid credentials!");
+    }
+
+    const isPasswordValid = await Password.compare(user.password, password);
+
+    if (!isPasswordValid) {
+      throw new BadRequestError("Invalid credentials!");
+    }
+
+    // Generate JWT and save it to the session object on cookie
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      JWT_SECRET
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(user);
+  }
+);
 
 export { router as signinRouter };
