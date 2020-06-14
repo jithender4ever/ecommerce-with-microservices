bff4b6f132f03622d1a44c095cceda3f6fd0d326
Jithender Reddy P <Jithender_R_Pakanati@homedepot.com>
2020-06-14 14:59:33 -0400

    Implemented middleware to validate requests.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -6,6 +6,7 @@ import { RequestValidationError } from "../errors/requestValidationError";
 import { User } from "../models/user";
 
 import { JWT_SECRET } from "../config";
+import { validateRequest } from "../middlewares/validateRequest";
 
 const router = express.Router();
 
@@ -18,12 +19,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
